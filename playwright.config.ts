e7be755b0861caa7195c933838e14c9511925a09
Sebastian Holtkamp <S.Holtkamp@conterra.de>
2025-03-06 07:47:22 +0100

    WIP

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './src/main/js/bundles/dn_what3words/tests',
+  testDir: './src/main/js/bundles/dn_what3words/tests/playwright',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
