93b8af1cc6268a39170d24bc5a5dab9bd8e85d4b
Sebastian Holtkamp <S.Holtkamp@conterra.de>
2025-07-22 11:43:34 +0200

    Update playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,79 +1,88 @@
-import { defineConfig, devices } from '@playwright/test';
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
+/*
+ * Copyright (C) con terra GmbH
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import {
+    defineConfig,
+    devices,
+    PlaywrightTestConfig
+} from "@playwright/test";
+import { env } from "process";
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
-export default defineConfig({
-  testDir: './src/main/js/bundles/dn_what3words/tests/playwright',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+const baseURL = env.BASE_URL || "http://localhost:9090";
+const testDir = env.TESTS_DIR || "./src/test/end-to-end/test";
+const outputDir = env.TARGET_DIR || "./target/playwright/output";
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
+const isCI = !!env.CI;
 
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+const snapshotsFolder = env.SNAPSHOTS_DIR || "./src/test/end-to-end/test/snapshots/local";
+
+const config: PlaywrightTestConfig = {
+    testDir,
+    outputDir: `${outputDir}/results`,
+
+    snapshotPathTemplate: `${snapshotsFolder}/{testFileName}-snapshots/{arg}-{projectName}{ext}`,
+
+    /* Run tests in files in parallel */
+    fullyParallel: true,
+    /* Retry on CI only */
+    retries: isCI ? 1 : 0,
+    /* Fail the build on CI if you accidentally left test.only in the source code. */
+    forbidOnly: isCI,
+    /* Opt out of parallel tests on CI. */
+    workers: isCI ? 1 : 2,
+
+    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+    use: {
+        /* Base URL to use in actions like `await page.goto('/')`. */
+        baseURL,
+
+        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+        trace: "on-first-retry"
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+    reporter: [
+        [
+            "html",
+            {
+                outputFolder: `${outputDir}/reports/html`,
+                open: isCI ? "never" : "always"
+            }
+        ],
+        ["junit", { outputFile: `${outputDir}/reports/junit/results.xml` }]
+    ],
+
+    expect: {
+        toHaveScreenshot: {
+            maxDiffPixelRatio: 0.1,
+            threshold: 0.05
+        },
+        // increased timeout (default is 5000)
+        // pro: less flaky tests, no need to wait for network state
+        // con: longer test duration where assertions are failing
+        timeout: 30000
     },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    /* Configure projects for major browsers */
+    projects: [
+        {
+            name: "Desktop Chrome",
+            use: {
+                ...devices["Desktop Chrome"],
+                launchOptions: {
+                    // enable WEBGL for headless tests
+                    args: isCI
+                        ? ["--disable-gpu"]
+                        : []
+                },
+                viewport: { width: 1920, height: 1080 }
+            }
+        }
+    ],
 
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
-});
+    timeout: isCI ? 120000 : 30000
+};
+export default defineConfig(config);
